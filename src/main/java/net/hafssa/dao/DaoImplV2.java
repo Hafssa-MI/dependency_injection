a03f5578b93cac169435fc3cfdda0e74b09c6b2b
HAFSSA <hafssa.miftah-etu@etu.univh2c.ma>
2026-02-27 22:47:31 +0000

    Etape 3: Ajouter une implémentation version 2 du DaoImpl2 dont on constate le besoin d'une instantiation dynamique de la classe presentation.

---
+++ src/main/java/net/hafssa/dao/DaoImplV2.java
@@ -0,0 +1,10 @@
+package net.hafssa.dao;
+
+public class DaoImplV2 implements IDao{
+    @Override
+    public double getData(){
+        System.out.println("Version Capteurs ..");
+        double t =12;
+        return t;
+    }
+}
--- src/main/java/net/hafssa/pres/pres1.java
+++ src/main/java/net/hafssa/pres/pres1.java
@@ -1,10 +1,12 @@
 package net.hafssa.pres;
 
 import net.hafssa.dao.DaoImpl;
+import net.hafssa.dao.DaoImplV2;
 import net.hafssa.metier.MetierImpl;
 
 public class pres1 {
     public static void main(String[] args) {
+        DaoImplV2 d2 = new DaoImplV2();
         DaoImpl d = new DaoImpl();
         /**
          * Injection des dépendances avec méthode statique : via constructeur ou setter
